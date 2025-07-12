ce4a5b76fff434d10ae0e53b62e8929931110a8f
sufiyan10-1 <134135723+sufiyan10-1@users.noreply.github.com>
2025-07-12 11:16:58 +0530

    added camera settings

--- src/components/xr-cube/XrCubeContainer.jsx
+++ src/components/xr-cube/XrCubeContainer.jsx
@@ -2,17 +2,22 @@ import { Canvas } from '@react-three/fiber'
 import { ARButton, XR } from '@react-three/xr'
 import XrCube from './XrCube'
 
-
 const CubeContainer = () => {
   return (
     <>
-    <ARButton/>
-    <Canvas>
-       <XR>
-        <XrCube/>
-      </XR>
-    </Canvas>
-  </>
+      <ARButton />
+      <Canvas
+        gl={{ antialias: true }}
+        onCreated={({ gl }) => {
+          gl.xr.enabled = true;
+        }}
+        camera={{ position: [0, 0, 0] }}
+      >
+        <XR sessionInit={{ requiredFeatures: ['hit-test'] }}>
+          <XrCube />
+        </XR>
+      </Canvas>
+    </>
   )
 }
 
