4d4755beb3ba6ae2a23de1a43f8db98110a5092d
sufiyan10-1 <134135723+sufiyan10-1@users.noreply.github.com>
2025-07-12 11:20:54 +0530

    update xr

--- src/components/xr-cube/XrCubeContainer.jsx
+++ src/components/xr-cube/XrCubeContainer.jsx
@@ -2,22 +2,17 @@ import { Canvas } from '@react-three/fiber'
 import { ARButton, XR } from '@react-three/xr'
 import XrCube from './XrCube'
 
+
 const CubeContainer = () => {
   return (
     <>
-      <ARButton />
-      <Canvas
-        gl={{ antialias: true }}
-        onCreated={({ gl }) => {
-          gl.xr.enabled = true;
-        }}
-        camera={{ position: [0, 0, 0] }}
-      >
-        <XR sessionInit={{ requiredFeatures: ['hit-test'] }}>
-          <XrCube />
-        </XR>
-      </Canvas>
-    </>
+    <ARButton/>
+    <Canvas>
+       <XR>
+        <XrCube/>
+      </XR>
+    </Canvas>
+  </>
   )
 }
 
